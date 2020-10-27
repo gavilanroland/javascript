87f6e0cb71042a6eb3ac5975bcaea519688775a4
gavilanroland <gavilanrolando@protonmail.com>
2020-10-27 09:06:46 -0300

    Excercise 1 week four

---
+++ js/app.js
@@ -0,0 +1,15 @@
+"use strict"
+/*Excercise 1 : Use of Numeric Variables*/
+var numberA = 366;
+var numberB = 234;
+var addAB =(numberA + numberB);
+/*Excersise 2: Use of string variables*/
+var name ="Rolando";
+var lastName ="Gavilan";
+var fullName =(name + lastName);
+/*Excersise 3 : string's sum*/
+var varA ="Ciudad";
+var varB ="BuenosAires";
+var sumVarAVarB = (varA + varB);
+console.log("La cadena de texto tiene :"+ sumVarAVarB.length + "caracteres");
+
